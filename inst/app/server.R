8965527460ac2387c798e34b480542470493091e
vjcitn <stvjc@channing.harvard.edu>
2022-03-02 16:30:29 -0500

    adding app

---
+++ inst/app/server.R
@@ -0,0 +1,24 @@
+
+suppressPackageStartupMessages({
+library(shiny)
+library(tnt4dn8)
+data(gtex_b38_lung_chr20_exc)
+})
+
+#' data(gtex_b38_lung_chr20_exc)
+#' cands = avail_syms_gtex()
+#' cands
+#' chk2 = gtex_b38_lung_chr20_exc |> filter_sym(cands[3], radius=5e5) |> as.data.frame()
+
+server = function(input, output, session) {
+  output$gname = renderPrint({
+    cat(input$gene)
+  })
+  output$tntmanh = renderTnT({
+   sym = input$gene
+   rad = input$radius
+   tab = gtex_b38_lung_chr20_exc |> filter_sym(sym, radius=rad) |> as.data.frame()
+   tntplot(tab)
+  })
+}
+  
---
+++ inst/app/ui.R
@@ -0,0 +1,21 @@
+suppressPackageStartupMessages({
+library(shiny)
+library(tnt4dn8)
+})
+
+ui = fluidPage(
+ sidebarLayout(
+  sidebarPanel(
+   helpText("tnt4dn8 demo"),
+   selectInput("gene", "gene", choices = sort(avail_syms_gtex())),
+   numericInput("radius", "radius", min=0, max=5e6, step=5e5, value=5e5),
+   width=2
+   ),
+  mainPanel(
+   tabsetPanel(
+    tabPanel("manh", verbatimTextOutput("gname"), TnTOutput("tntmanh")),
+    tabPanel("about", helpText("simple demonstration, small set of genes on chr20, GTEx lung excerpt"))
+   )
+  )
+ )
+)
