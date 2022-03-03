2c2e574a6bc653886f884ffa4291a26289feb2c6
vjcitn <stvjc@channing.harvard.edu>
2022-03-02 19:43:51 -0500

    bump

--- R/viz.R
+++ R/viz.R
@@ -71,18 +71,20 @@ tntplot = function(tab, snpcolor="lightblue", genecolor="gold", gt=NULL, maxp = 
 tntplot2 = function(tab, snpcolor="lightblue", genecolor="gold", gt=NULL, maxp = .1) {
   if (!requireNamespace("TnT")) stop("install TnT to use this")
   if (!requireNamespace("GenomicRanges")) stop("install GenomicRanges to use this")
+  txdb19 = TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene
+  odb = org.Hs.eg.db::org.Hs.eg.db
 # GENE
-  GT = TnT::GeneTrackFromTxDb(TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene,
+  GT = TnT::GeneTrackFromTxDb(txdb19,
       height=100, color=genecolor)  # consider making this optionally passed as a fixed object
   suppressMessages({
-    syms = AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db, keys=GT@Data$id, keytype="ENTREZID", column="SYMBOL")
+    syms = AnnotationDbi::mapIds(odb, keys=GT@Data$id, keytype="ENTREZID", column="SYMBOL")
   })
   GT@Data$display_label = TnT::strandlabel(syms, GenomicRanges::strand(GT@Data))
 # TRANSCRIPT
-  if (is.null(gt)) gt = TnT::TxTrackFromTxDb(TxDb.Hsapiens.UCSC.hg19.knownGene::TxDb.Hsapiens.UCSC.hg19.knownGene,
+  if (is.null(gt)) gt = TnT::TxTrackFromTxDb(txdb19,
       height=400, color=genecolor)  # consider making this optionally passed as a fixed object
   suppressMessages({
-    syms = gt@Data$tooltip$tx_name #AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db, keys=gt@Data$id, keytype="ENTREZID", column="SYMBOL")
+    syms = gt@Data$tooltip$tx_name 
   })
   gt@Data$display_label = TnT::strandlabel(syms, GenomicRanges::strand(gt@Data))
   if (!is.null(maxp)) {
@@ -92,6 +94,6 @@ tntplot2 = function(tab, snpcolor="lightblue", genecolor="gold", gt=NULL, maxp =
   t2g = tab2grngs(tab)
   tab$value = t2g$value
   pt = TnT::PinTrack( t2g, height=200, tooltip = as.data.frame(tab), color=snpcolor )
-  TnT::TnTGenome(list(pt, GT, gt), view.range=(range(t2g)+10000), coord.range=GenomicRanges::ranges(range(t2g)+5e6)[1])
+  TnT::TnTGenome(list(pt, GT, gt), view.range=(range(t2g)+10000), coord.range=GenomicRanges::ranges(range(t2g)+1e5)[1])
 }
     
